c534737cd9c267fe9f044686147dc9feecf25efb
Yifan Zhu <llag9810@outlook.com>
2018-07-04 17:01:46 +0800

    feat: add network discover

---
+++ app/src/main/java/com/unique/eightzeroeight/wishare/network/client/ClientConfig.java
@@ -0,0 +1,17 @@
+package com.unique.eightzeroeight.wishare.network.client;
+
+/**
+ * Created by amyli on 2017/2/15.
+ */
+
+public class ClientConfig {
+    private static int askFunc;
+
+    public static int getAskFunc() {
+        return askFunc;
+    }
+
+    public static void setAskFunc(int func) {
+        askFunc = func;
+    }
+}
